2fd3c8cfa9c909940f6a3d5e07ceeb5aebf56a9c
Vapurrmaid <vapurrmaid@gmail.com>
2020-02-15 16:33:39 -0500

    Change wait time for live-server

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -6,5 +6,5 @@ set -eou pipefail
 pushd $(git rev-parse --show-toplevel)
 ./scripts/build.sh --dev-mode
 ./node_modules/.bin/watch 'yarn build --dev-mode' 'src' &
-./node_modules/.bin/live-server --entry-file=index.html --no-css-inject --port=8080 --wait=1000 dist
+./node_modules/.bin/live-server --entry-file=index.html --no-css-inject --port=8080 --wait=300 dist
 popd
